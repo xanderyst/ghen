d25757e8267c6425ac4d44a6e46531d3ec72e3c4
Alexander Tseng <xanderyst@gmail.com>
2024-05-14 02:29:01 -0700

    polish the keyboard and icons

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,8 @@ import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 
 export const metadata = {
-    title: "Promptopia",
-    description: 'Discover & Share AI Prompts'
+    title: "Ghen",
+    description: 'Learn Chinese Characters Daily'
 }
 
 const RootLayout = ({ children }) => {
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
       <Link href="/" className="flex gap-2 flex-center">
         <Image 
           alt="Promptopia Logo"
-          src="/assets/images/logo.svg"
+          src="/assets/images/translate.svg"
           width={30}
           height={30}
           className="object-contain"
