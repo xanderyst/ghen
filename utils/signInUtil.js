01a842c2b53469ffb344d6868616368caa4f1f3e
Alexander Tseng <xanderyst@gmail.com>
2024-05-30 03:21:07 -0700

    only display alert when using webview

--- utils/signInUtil.js
+++ utils/signInUtil.js
@@ -2,7 +2,6 @@ import isWebview from 'is-ua-webview';
 import { signIn } from 'next-auth/react';
 
 export async function signInToMyApp(providerId) {
-    alert("Open in Safari or Chrome to sign in");
     if (isWebview(window.navigator.userAgent)) {
       alert("Open in Safari or Chrome to sign in");
       return;
